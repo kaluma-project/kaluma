521506b9fbfaaf31a2dc45f31d7d50d8166b0602
Jay Kim <jayden.kim.biz@gmail.com>
2020-01-03 13:22:37 -0800

    Storage functions  - Changes for the issue #180

--- include/port/storage.h
+++ include/port/storage.h
@@ -37,20 +37,26 @@ int storage_clear();
 int storage_length();
 
 /**
+ * Return the number of empty items in the storage
+ * @return The number of items, or -1 on failture
+ */
+int storage_space();
+
+/**
  * Get value of key index
  * @param key The point to key string
  * @param buf The pointer to the buffer to store value
  * @param size The size of the buffer
  * @return Returns the length of value or -1 on failure (key not found)
  */
-int storage_get_item(const char *key, char *buf, int size);
+int storage_get_item(const char *key, char *buf, int *size);
 
 /**
  * Set the value with a key string
  * @param key The point to key string
  * @param buf The pointer to the buffer to store value
  * @param size The size of the buffer
- * @return Returns 0 on success or -1 on failure.
+ * @return Returns 0 on success or -1 on failure or -2 on no empty space.
  */
 int storage_set_item(const char *key, char *buf, int size);
 
@@ -68,6 +74,6 @@ int storage_remove_item(const char *key);
  * @param size The size of the buffer
  * @return Returns 0 on success or -1 on failure.
  */
-int storage_key(const int index, char *buf, int size);
+int storage_key(const int index, char *buf, int *size);
 
 #endif /* __STORAGE_H */
