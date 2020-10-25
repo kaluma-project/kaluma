38246c40b55507243cdcd938c6708c77ec96d15c
Jay Kim <jayden.kim.biz@gmail.com>
2020-10-25 14:26:09 -0700

    SPI driver  - #227

--- esp32/main/targets/boards/esp32-devkitc/src/spi.c
+++ esp32/main/targets/boards/esp32-devkitc/src/spi.c
@@ -25,10 +25,12 @@
 #include <driver/spi_master.h>
 #include <string.h>
 
-#define GPIO_MOSI 12
-#define GPIO_MISO 13
-#define GPIO_SCLK 15
-#define GPIO_CS 14
+#define GPIO_MOSI_0 5
+#define GPIO_MISO_0 18
+#define GPIO_SCLK_0 19
+#define GPIO_MOSI_1 27
+#define GPIO_MISO_1 26
+#define GPIO_SCLK_1 25
 
 void spi_init()
 {
@@ -36,22 +38,31 @@ void spi_init()
 
 void spi_cleanup()
 {
+  for (int k = 0; k < SPI_NUM; k++) {
+    spi_close(k);
+  }
 }
 
-static spi_device_handle_t handle;
+static spi_device_handle_t handle[SPI_NUM];
 
 int spi_setup(uint8_t bus, spi_mode_t mode, uint32_t baudrate, spi_bitorder_t bitorder)
 {
     esp_err_t ret;
     //Configuration for the SPI bus
     spi_bus_config_t buscfg={
-        .mosi_io_num=GPIO_MOSI,
-        .miso_io_num=GPIO_MISO,
-        .sclk_io_num=GPIO_SCLK,
+        .mosi_io_num=GPIO_MOSI_0,
+        .miso_io_num=GPIO_MISO_0,
+        .sclk_io_num=GPIO_SCLK_0,
         .quadwp_io_num=-1,
-        .quadhd_io_num=-1
+        .quadhd_io_num=-1,
+        .flags = SPICOMMON_BUSFLAG_MASTER,
     };
-
+    if (bus == 1)
+    {
+        buscfg.mosi_io_num=GPIO_MOSI_1;
+        buscfg.miso_io_num=GPIO_MISO_1;
+        buscfg.sclk_io_num=GPIO_SCLK_1;
+    }
     //Configuration for the SPI device on the other side of the bus
     spi_device_interface_config_t devcfg={
         .command_bits=0,
@@ -59,16 +70,19 @@ int spi_setup(uint8_t bus, spi_mode_t mode, uint32_t baudrate, spi_bitorder_t bi
         .dummy_bits=0,
         .clock_speed_hz=baudrate,
         .duty_cycle_pos=128,        //50% duty cycle
-        .mode=2,
-        .spics_io_num=GPIO_CS,
+        .mode=mode,
+        .spics_io_num= -1,
         .cs_ena_posttrans=3,        //Keep the CS low 3 cycles after transaction, to stop slave from missing the last bit when CS has less propagation delay than CLK
         .queue_size=3
     };
+    if (bitorder == SPI_BITORDER_LSB) {
+        devcfg.flags = SPI_DEVICE_BIT_LSBFIRST;
+    }
     ret=spi_bus_initialize(HSPI_HOST, &buscfg, 1);
     assert(ret==ESP_OK);
-    ret=spi_bus_add_device(HSPI_HOST, &devcfg, &handle);
+    ret=spi_bus_add_device(HSPI_HOST, &devcfg, &handle[bus]);
     assert(ret==ESP_OK);
-  return 0;
+  return ret;
 }
 
 int spi_sendrecv(uint8_t bus, uint8_t *tx_buf, uint8_t *rx_buf, size_t len, uint32_t timeout)
@@ -79,8 +93,8 @@ int spi_sendrecv(uint8_t bus, uint8_t *tx_buf, uint8_t *rx_buf, size_t len, uint
     t.length=len*8;
     t.tx_buffer=tx_buf;
     t.rx_buffer=rx_buf;
-    ret=spi_device_transmit(handle, &t);
-    return 0;
+    ret=spi_device_transmit(handle[bus], &t);
+    return ret;
 }
 
 int spi_send(uint8_t bus, uint8_t *buf, size_t len, uint32_t timeout)
@@ -90,8 +104,8 @@ int spi_send(uint8_t bus, uint8_t *buf, size_t len, uint32_t timeout)
     memset(&t, 0, sizeof(t));
     t.length=len*8;
     t.tx_buffer=buf;
-    ret=spi_device_polling_transmit(handle, &t);
-    return 0;
+    ret=spi_device_polling_transmit(handle[bus], &t);
+    return ret;
 }
 
 int spi_recv(uint8_t bus, uint8_t *buf, size_t len, uint32_t timeout)
@@ -101,14 +115,14 @@ int spi_recv(uint8_t bus, uint8_t *buf, size_t len, uint32_t timeout)
     memset(&t, 0, sizeof(t));
     t.length=len*8;
     t.rx_buffer=buf;
-    ret=spi_device_transmit(handle, &t);
-    return 0;
+    ret=spi_device_transmit(handle[bus], &t);
+    return ret;
 }
 
 int spi_close(uint8_t bus)
 {
     esp_err_t ret;
-    ret=spi_bus_remove_device(handle);
-    return 0;
+    ret=spi_bus_remove_device(handle[bus]);
+    return ret;
 }
 
