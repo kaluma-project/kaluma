62c2994b8e9c529e4a5a12587721e6cd5859e5ca
Jay Kim <jayden.kim.biz@gmail.com>
2021-02-02 21:19:24 -0800

    Make unify build script using CMAKE  - Remove EPS 32 project which is not supported anymore  - Remove GNU Makefile  - Create new CMAKE build script for kameleon-core and Llinux  - #223

--- esp32/README.md
+++
@@ -1,5 +0,0 @@
-# Build process
-
-1. `make TARGET=esp32-devkitc` to generate esp32-module
-2. run `esp32/components/jerryscript/build.sh` to build jerry script for esp32
-3. run `idf.py build` in esp32 directory.
--- tools/kameleon.cmake
+++ tools/kameleon.cmake
@@ -1,8 +1,14 @@
-project(kameleon C)
- 
-include(../targets/boards/esp32-devkitc/target.cmake)
+######################################
+# building variables
+######################################
+# debug build?
+set(DEBUG 1)
+# optimization
+set(OPT -Og)
 
-set(JERRY_ROOT ${CMAKE_SOURCE_DIR}/../deps/jerryscript)
+include(${CMAKE_SOURCE_DIR}/targets/boards/${TARGET}/target.cmake)
+
+set(JERRY_ROOT ${CMAKE_SOURCE_DIR}/deps/jerryscript)
 set(JERRY_INC
   ${JERRY_ROOT}/jerry-core
   ${JERRY_ROOT}/jerry-core/api
@@ -23,11 +29,8 @@ set(JERRY_INC
   ${JERRY_ROOT}/jerry-ext/include
   ${JERRY_ROOT}/jerry-libm)
 
-
-#===========================================
-# TODO
 set(JERRY_ARGS
-  --toolchain=cmake/toolchain_mcu_stm32f4.cmake
+  --toolchain=cmake/${JERRY_TOOLCHAIN}
   --lto=OFF
   --error-messages=ON
   --js-parser=ON
@@ -40,31 +43,42 @@ set(JERRY_ARGS
   --jerry-cmdline=OFF
   --cpointer-32bit=ON)
 
+set(SRC_DIR ${CMAKE_SOURCE_DIR}/src)
+set(KAMELEON_GENERATED_C
+  ${SRC_DIR}/gen/kameleon_modules.c
+  ${SRC_DIR}/gen/kameleon_magic_strings.c)
+set(KAMELEON_GENERATED_H
+  ${SRC_DIR}/gen/kameleon_modules.h
+  ${SRC_DIR}/gen/kameleon_magic_strings.h)
+
+set(KAMELEON_GENERATED ${KAMELEON_GENERATED_C} ${KAMELEON_GENERATED_H})
+
+string (REPLACE ";" " " KAMELEON_MODULE_LIST "${KAMELEON_MODULES}")
+
 set(JERRY_LIBS
   ${JERRY_ROOT}/build/lib/libjerry-core.a
   ${JERRY_ROOT}/build/lib/libjerry-ext.a)
 
 add_custom_command(OUTPUT ${JERRY_LIBS}
+  DEPENDS ${KAMELEON_GENERATED_C}
   WORKING_DIRECTORY ${JERRY_ROOT}
   COMMAND python tools/build.py --clean ${JERRY_ARGS})
-  
+
+add_custom_command(OUTPUT ${KAMELEON_GENERATED_C}
+  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+  COMMAND python ${JERRY_ROOT}/tools/build.py --clean --jerry-cmdline-snapshot=ON --snapshot-save=ON --snapshot-exec=ON --profile=es2015-subset
+  COMMAND node tools/js2c.js --modules=${KAMELEON_MODULE_LIST} --target=${TARGET}
+  COMMAND rm -rf deps/jerryscript/build)
+
+# Delete generaged file to generate it every time.
+add_custom_command(OUTPUT clean_gen
+  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+  COMMAND rm -f src/gen/*)
 #=============================================================
-set(PREFIX arm-none-eabi-)
-set(CMAKE_ASM_COMPILER ${PREFIX}gcc)
-set(CMAKE_C_COMPILER ${PREFIX}gcc)
-set(CMAKE_CXX_COMPILER ${PREFIX}g++)
-set(CMAKE_LINKER ${PREFIX}ld)
-set(CMAKE_OBJCOPY ${PREFIX}objcopy)
-
-
-set(SRC_DIR ../src)
-set(KAMELEON_INC ../include ../include/port ${SRC_DIR}/gen ${SRC_DIR}/modules)
+set(KAMELEON_INC ${CMAKE_SOURCE_DIR}/include ${CMAKE_SOURCE_DIR}/include/port ${SRC_DIR}/gen ${SRC_DIR}/modules)
 include_directories(${KAMELEON_INC} ${JERRY_INC})
 
 list(APPEND SOURCES
-  ${SRC_DIR}/main.c
-  ${SRC_DIR}/gen/kameleon_modules.c
-  ${SRC_DIR}/gen/kameleon_magic_strings.c
   ${SRC_DIR}/main.c
   ${SRC_DIR}/utils.c
   ${SRC_DIR}/base64.c
@@ -74,19 +88,20 @@ list(APPEND SOURCES
   ${SRC_DIR}/jerry_port.c
   ${SRC_DIR}/jerryxx.c
   ${SRC_DIR}/global.c
-  ${SRC_DIR}/ymodem.c)
+  ${SRC_DIR}/ymodem.c
+  ${KAMELEON_GENERATED_C})
 
 if(KAMELEON_MODULE_PWM)
   list(APPEND SOURCES ${SRC_DIR}/modules/pwm/module_pwm.c)
-  include_directories(${SRC_DIR}/modules/pwm)  
+  include_directories(${SRC_DIR}/modules/pwm)
 endif()
 if(KAMELEON_MODULE_I2C)
   list(APPEND SOURCES ${SRC_DIR}/modules/i2c/module_i2c.c)
-  include_directories(${SRC_DIR}/modules/i2c)  
+  include_directories(${SRC_DIR}/modules/i2c)
 endif()
 if(KAMELEON_MODULE_SPI)
   list(APPEND SOURCES ${SRC_DIR}/modules/spi/module_spi.c)
-  include_directories(${SRC_DIR}/modules/spi)  
+  include_directories(${SRC_DIR}/modules/spi)
 endif()
 if(KAMELEON_MODULE_STORAGE)
   list(APPEND SOURCES ${SRC_DIR}/modules/storage/module_storage.c)
@@ -104,15 +119,15 @@ if(KAMELEON_MODULE_GRAPHICS)
     ${SRC_DIR}/modules/graphics/gc.c
     ${SRC_DIR}/modules/graphics/font_default.c
     ${SRC_DIR}/modules/graphics/module_graphics.c)
-  include_directories(${SRC_DIR}/modules/graphics)  
+  include_directories(${SRC_DIR}/modules/graphics)
 endif()
 
-add_executable(kameleon-core.elf ${SOURCES} ${JERRY_LIBS})
-target_link_libraries(kameleon-core.elf ${JERRY_LIBS} c nosys m)
+add_executable(${TARGET}.elf ${SOURCES} ${JERRY_LIBS} clean_gen)
+target_link_libraries(${TARGET}.elf ${JERRY_LIBS} ${TARGET_LIBS})
 
-add_custom_command(OUTPUT kameleon-core.hex kameleon-core.bin
-  COMMAND ${CMAKE_OBJCOPY} -O ihex kameleon-core.elf kameleon-core.hex
-  COMMAND ${CMAKE_OBJCOPY} -O binary -S kameleon-core.elf kameleon-core.bin
-  DEPENDS kameleon-core.elf)
+add_custom_command(OUTPUT ${TARGET}.hex ${TARGET}.bin
+  COMMAND ${CMAKE_OBJCOPY} -O ihex ${TARGET}.elf ${TARGET}.hex
+  COMMAND ${CMAKE_OBJCOPY} -O binary -S ${TARGET}.elf ${TARGET}.bin
+  DEPENDS ${TARGET}.elf)
 
-add_custom_target(kameleon ALL DEPENDS kameleon-core.hex kameleon-core.bin)
+add_custom_target(kameleon ALL DEPENDS ${TARGET}.hex ${TARGET}.bin)
