726831ff06577684d445747a1a641d6bbd740410
Jay Kim <changjae.kim@gmail.com>
2021-11-07 01:19:33 -0700

    Bug fix - out_enable default is 0

--- src/modules/rp2/module_rp2.c
+++ src/modules/rp2/module_rp2.c
@@ -179,7 +179,7 @@ JERRYXX_FUN(pio_sm_init_fn) {
   // setup out special
   bool out_sticky = (uint8_t)jerryxx_get_property_boolean(
       options, MSTR_RP2_PIO_SM_OUT_STICKY, false);
-  int out_enable_pin = (uint8_t)jerryxx_get_property_number(
+  int out_enable_pin = (int8_t)jerryxx_get_property_number(
       options, MSTR_RP2_PIO_SM_OUT_ENABLE_PIN, -1);
   sm_config_set_out_special(&sm_config, out_sticky, (out_enable_pin > -1),
                             (out_enable_pin > -1) ? out_enable_pin : 0);
