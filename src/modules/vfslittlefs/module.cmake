0138179c1109c6cc67f2e5fccecf1b198be78939
Minkyu Lee <niklaus.lee@gmail.com>
2021-11-28 12:46:26 +0900

    add littlefs to cmake build

--- src/modules/vfslittlefs/module.cmake
+++ src/modules/vfslittlefs/module.cmake
@@ -1,2 +1,8 @@
-list(APPEND SOURCES ${SRC_DIR}/modules/vfslittlefs/module_vfslittlefs.c)
-include_directories(${SRC_DIR}/modules/vfslittlefs)
+set(LITTLEFS_DIR ${CMAKE_SOURCE_DIR}/lib/littlefs)
+list(APPEND SOURCES
+  ${LITTLEFS_DIR}/lfs.c
+  ${LITTLEFS_DIR}/lfs_util.c
+  ${SRC_DIR}/modules/vfslittlefs/module_vfslittlefs.c)
+include_directories(
+  ${LITTLEFS_DIR}
+  ${SRC_DIR}/modules/vfslittlefs)
