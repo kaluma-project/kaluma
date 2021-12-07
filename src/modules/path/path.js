e931c7c09b246282b2c605e46e6ae4c5ebc71682
Minkyu Lee <niklaus.lee@gmail.com>
2021-12-07 16:31:20 +0900

    Added 'path' module

--- src/modules/path/path.js
+++ src/modules/path/path.js
@@ -2,7 +2,7 @@ const sep = "/";
 const delimiter = ":";
 
 function isAbsolute(path) {
-  return path.startsWith("/");
+  return path.startsWith(sep);
 }
 
 function format(pathObj) {
@@ -22,23 +22,76 @@ function format(pathObj) {
     : `${pathObj.root}${_base}`;
 }
 
-function parse() {
-  return sep;
+function parse(path) {
+  const _terms = path.split(sep).filter(t => t.length > 0);
+  const _base = _terms.pop() || '';
+  const _bt = _base.split('.');
+  const _ext = _bt.pop() || '';
+  const _name = _bt.join('.');
+  let pathObj = {
+    root: isAbsolute(path) ? sep : '',
+    dir: _terms.join(sep),
+    base: _base,
+    name: _name,
+    ext: '.' + _ext
+  }
+  return pathObj;
 }
 
-function join() {}
+function dirname(path) {
+  const _po = parse(path);
+  return _po.root + _po.dir;
+}
 
-function dirname() {}
+function extname(path) {
+  return parse(path).ext;
+}
 
-function extname() {}
+function basename(path, ext) {
+  const _po = parse(path);
+  return _po.ext === ext ? _po.name : _po.base;
+}
 
-function basename() {}
+function normalize(path) {
+  const _terms = path.split(sep).filter(t => t.length > 0);
+  let _paths = [];
+  if (path.startsWith(sep)) _paths.push('');
+  for (let i = 0; i < _terms.length; i++) {
+    const _t = _terms[i];
+    if (_t === '.') {
+      // do nothing
+    } else if (_t === '..') {
+      _paths.pop();
+    } else {
+      _paths.push(_t);
+    }
+  }
+  return _paths.join(sep);
+}
 
-function relative() {}
+function join(...paths) {
+  let _terms = [];
+  if (paths.length > 0 && paths[0].startsWith(sep)) _terms.push('');
+  for (let i = 0; i < paths.length; i++) {
+    const _p = paths[i];
+    _terms = _terms.concat(_p.split(sep).filter(t => t.length > 0));
+  }
+  return normalize(_terms.join(sep));
+}
 
-function resolve() {}
-
-function normalize() {}
+function resolve(...paths) {
+  const _cwd = global.__cwd || sep;
+  let _dir = _cwd;
+  for (let i = 0; i < paths.length; i++) {
+    const _path = paths[i];
+    if (isAbsolute(_path)) {
+      _dir = normalize(_path);
+    } else {
+      _dir = join(_dir, _path);
+    }
+  }
+  return _dir;
+}
 
 exports.isAbsolute = isAbsolute;
 exports.format = format;
@@ -47,7 +100,6 @@ exports.join = join;
 exports.dirname = dirname;
 exports.extname = extname;
 exports.basename = basename;
-exports.relative = relative;
 exports.resolve = resolve;
 exports.normalize = normalize;
 exports.sep = sep;
