25031822fa1948e0bb86e56d8bbf4aef2677ac62
Minkyu Lee <niklaus.lee@gmail.com>
2021-12-19 17:55:02 +0900

    File system in flash is cleared after program flash #463

--- targets/rp2/boards/pico/board.js
+++ targets/rp2/boards/pico/board.js
@@ -6,5 +6,6 @@ global.board.LED = 25;
 const fs = require("fs");
 const { VFSLittleFS } = require("vfs_lfs");
 fs.register("lfs", VFSLittleFS);
-const bd = new global.Flash(0, 128);
+// start block is 4(storage) + 128(program)
+const bd = new global.Flash(132, 128);
 fs.mount("/", bd, "lfs", true);
