faa67de1ad1f86b2b9b506af7db8c7f2c34dd449
tomasmcm <tomasmcm@whitesmith.co>
2022-07-09 19:15:30 +0100

    add pico_w board

---
+++ targets/rp2/boards/pico_w/board.js
@@ -0,0 +1,12 @@
+// initialize board object
+global.board.name = "picow";
+// global.board.LED = "WL_GPIO0";
+
+// mount lfs on "/"
+const fs = require("fs");
+const { VFSLittleFS } = require("vfs_lfs");
+const { Flash } = require("flash");
+fs.register("lfs", VFSLittleFS);
+// fs block starts after 4(storage) + 128(program)
+const bd = new Flash(132, 128);
+fs.mount("/", bd, "lfs", true);
