92df1029111aa9eb3faeaff18c13501a3d27d903
Minkyu Lee <niklaus.lee@gmail.com>
2023-01-04 14:50:02 +0900

    Setup ieee80211 and network drivers for Pico-W

--- targets/rp2/boards/pico-w/board.js
+++ targets/rp2/boards/pico-w/board.js
@@ -1,6 +1,5 @@
 // initialize board object
 global.board.name = "pico-w";
-// global.board.LED = -1; // WLGP0
 
 // mount lfs on "/"
 const fs = require("fs");
@@ -10,3 +9,8 @@ fs.register("lfs", VFSLittleFS);
 // fs block starts after 4(storage) + 128(program)
 const bd = new Flash(132, 128);
 fs.mount("/", bd, "lfs", true);
+
+// setup ieee80211 and network drivers
+const { PicoCYW43WIFI, PicoCYW43Network } = require("pico_cyw43");
+global.__ieee80211dev = new PicoCYW43WIFI();
+global.__netdev = new PicoCYW43Network();
