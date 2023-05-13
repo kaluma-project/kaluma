c0b7f5c26d591d724d0c568d2fe906fa7c220551
Steve Shreeve <steve.shreeve@gmail.com>
2023-05-13 00:24:12 -0600

    Use all cores when building

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 // Generate c source from js files
 
 const fs = require("fs-extra");
+const os = require("os");
 const path = require("path");
 const minimist = require("minimist");
 const childProcess = require("child_process");
@@ -36,8 +37,10 @@ function build() {
   if (argv.modules) params.push(`-DMODULES=${argv.modules}`);
   if (argv.rev) params.push(`-DREV=-${argv.rev}`);
 
+  // build everything
+  const cores = os.cpus().length;
   cmd("cmake", params);
-  cmd("make");
+  cmd("make", [`-j${cores}`]);
   process.chdir(__dirname);
 }
 
